79a5c89f6eb2d92209a835127208f98d2406f6c3
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-07-16 15:18:43 +0300

    commit tree 696a3424422382ef4289fa1337ee247762e7f29d

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    return (
+        <input 
+            type='password' 
+            placeholder="Enter Password" 
+            className="textbox"
+            onChange={() => console.log("Entering password...")}
+        />
+        
+    )
+}
+
+export default Keypad;
